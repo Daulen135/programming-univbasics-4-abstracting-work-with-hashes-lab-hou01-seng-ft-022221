53096a9c7ce91e9428b7441e1de9d5208309a8c8
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-04 04:01:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,11 @@ my_hash_creator(1, 2)
 
 def read_from_hash(hash_to_read,key)
   
-  hash_to_read={name:"Steve"}
-  {:id=>name}
+  hash_to_read[key]
   
 end
-read_from_hash("Steve")
-read_from_hash("Tzvi")
+read_from_hash(hash_to_read,"Steve")
+read_from_hash(hash_to_read,"Tzvi")
 
 =begin
  describe "read_from_hash" do
