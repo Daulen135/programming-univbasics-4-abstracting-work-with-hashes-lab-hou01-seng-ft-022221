4d81e01662fd4b2742d843269d492958cf47b76d
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-04 02:26:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,9 +6,21 @@ my_hash_creator(1, 2)
 
 
 def read_from_hash(key)
-  {:id=>key}
+  {id=>key}
   
 end
 read_from_hash("Steve")
 read_from_hash("Tzvi")
 
+=begin
+ describe "read_from_hash" do
+    it "returns the value corresponding to the provided key" do
+      expect(read_from_hash({name: 'Steve'}, :name)).to eq('Steve')
+      expect(read_from_hash({'name' => 'Tzvi'}, 'name')).to eq('Tzvi')
+    end
+
+    it "returns nil if the key is not found in the provided hash" do
+      expect(read_from_hash({age: 31}, :name)).to eq(nil)
+    end
+  end
+  =end
